db006505c69e0bd058502434b19a37d8f2ef0240
Boris Vasilyev <1024sparrow@gmail.com>
2023-04-14 05:32:33 +0300

    winbuild.sh

--- winbuild.sh
+++ winbuild.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 # https://github.com/mdimura/docker-mingw-arch
 
-pushd $(dirname $0)
+function ERROR {
+	echo "Error: $1"
+	exit 1
+}
+
+if [ "$INDOCKER" == true ]
+then
+	cd /opt/src
 	if [ -d winbuild ]
 	then
 		rm -rf winbuild
 	fi
-	docker run --rm -it -v $PWD:/opt/src burningdaylight/mingw-arch:qt /bin/bash -c "cd /opt/src && mkdir winbuild && cd winbuild && x86_64-w64-mingw32-cmake .. && make"
-popd
+	mkdir winbuild
+	cd winbuild
+	x86_64-w64-mingw32-cmake .. && make || ERROR 'Ошибка сборки'
+	for iExe in $(find . -name *.exe)
+	do
+		echo "
+	$iExe"
+		for iDll in $(strings $iExe | grep -i '\.dll$')
+		do
+			for iSrc in $(find /usr/x86_64-w64-mingw32 -name $iDll)
+			do
+				echo "$iSrc"
+				cp $iSrc $(dirname $iExe)/ || ERROR 'Не удалось скопировать библиотеку зависимости'
+			done
+		done
+	done
+else
+	pushd $(dirname $0) > /dev/null
+		if [ -d winbuild ]
+		then
+			rm -rf winbuild
+		fi
+		docker run --rm -it -v $PWD:/opt/src -e INDOCKER=true burningdaylight/mingw-arch:qt /bin/bash /opt/src/winbuild.sh
+	popd > /dev/null
+fi
